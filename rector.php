10dc9cc864dc3a1ba7d9be6ff6511129d3a3dc4c
Petr Král <kral.petr.88@gmail.com>
2026-01-25 19:16:45 +0100

    chore: update composer.json file with run some tools in parallel mode

--- rector.php
+++ rector.php
@@ -13,4 +13,5 @@ return static function (RectorConfig $rectorConfig): void {
     ]);
 
     $rectorConfig->import(__DIR__ . '/vendor/pekral/rector-rules/rector.php');
+    $rectorConfig->parallel();
 };
